6300c1f7e891d74c7c42624786b72dc44834c2b5
Sergius Nyah <sergiusnyah@gmail.com>
2024-03-18 23:16:27 +0100

    But I'm not

--- client/js/toggle.js
+++ client/js/toggle.js
@@ -5,12 +5,16 @@ document.getElementById('theme-switcher').addEventListener('click', function() {
     if (body.classList.contains('light-mode')) {
         body.classList.remove('light-mode');
         body.classList.add('dark-mode');
-        icon.classList.remove('fa-sun');
-        icon.classList.add('fa-moon');
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
     } else {
         body.classList.remove('dark-mode');
         body.classList.add('light-mode');
-        icon.classList.remove('fa-moon');
-        icon.classList.add('fa-sun');
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
     }
 });
+
+//set initial state
+document.body.classList.add('light-mode');
+document.querySelector('#theme-switcher i').classList.add('fa-moon');

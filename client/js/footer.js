5ffa584d5e1946105c668a91309c833038762b88
Sergius Nyah <sergiusnyah@gmail.com>
2024-03-26 02:42:52 +0100

    Footer Biolerplate

---
+++ client/js/footer.js
@@ -0,0 +1,13 @@
+//Footer code
+function loadFooter(){
+    document.querySelector('footer').innerHTML = `
+    <div class="footer">
+        <div class="footer-text">
+        <a href="index.php">Tech Evangelist.</a>
+            <p>Made with ♡ from Earth 237 </p>
+        </div>
+    </div>
+    `;
+
+}
+loadFooter();
